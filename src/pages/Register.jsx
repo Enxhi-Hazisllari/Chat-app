0c797ddeef7806ca397004e08519278edd46fa13
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-04-12 16:16:35 +0200

    update style

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-
+import logo from '../../public/image/logo.png';
+import add from '../../public/image/addAvatar.png';
 const Register = () => {
   return (
     <div className='formContaner'>
       <div className="formWrapper">
-        <span className="logo">Enxhi Chat</span>
-        <span className="title">Register</span>
-        <form action="">
+        <img src={logo} alt="logo-chat" className='logo' />
+        <span className="title"> Enxhi Chat</span>
+        <span className="subtitle">Register</span>
+        <form>
           <input type="text" placeholder='Name'/>
           <input type="email" placeholder='Email'/>
           <input type="password" placeholder='Password'/>
-          <input type="file" />
+          <input style={{display:'none'}} type="file" id='file'/>
+          <label htmlFor="file">
+           <img src={add} alt="avatar-image" />
+           <span>+ Add avatar</span>
+          </label>
           <button>Sign up</button>
         </form>
         <p>You do have an account? Login</p>
