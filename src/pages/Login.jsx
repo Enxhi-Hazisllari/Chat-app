2edf9e50a3ba5d6309c1130985f3036372e7b2ad
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-04-12 16:27:24 +0200

    update Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import logo from '../../public/image/logo.png';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='formContaner'>
+    <div className="formWrapper">
+      <img src={logo} alt="logo-chat" className='logo' />
+      <span className="title"> Enxhi Chat</span>
+      <span className="subtitle">Login</span>
+      <form>
+        <input type="email" placeholder='Email'/>
+        <input type="password" placeholder='Password'/>
+        <button>Sign in</button>
+      </form>
+      <p>You don't have an account? Sign up</p>
+    </div>
+  </div>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../../public/image/logo.png';
 import add from '../../public/image/addAvatar.png';
+
 const Register = () => {
   return (
     <div className='formContaner'>
