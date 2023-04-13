f62d12a410ba74892f38a082eaef2aa1ad5ad899
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-04-13 17:55:29 +0200

    update Chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import Cam from '/image/cam.png'
+import Add from '/image/add.png'
+import More from '/image/more.png'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>Name</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="camera-img" />
+          <img src={Add} alt="add-img" />
+          <img src={More} alt="more-img" />
+        </div>
+      </div>  
+    </div>
   )
 }
 
