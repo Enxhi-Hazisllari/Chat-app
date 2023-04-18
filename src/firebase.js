c1408034f2a3ae865f9182c958acf757a1caa9dd
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-04-18 12:54:55 +0200

    create firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSsmQRiKQe403s5Xjsi0T8oDxt67mZeN8",
+  authDomain: "enxhi-chat.firebaseapp.com",
+  projectId: "enxhi-chat",
+  storageBucket: "enxhi-chat.appspot.com",
+  messagingSenderId: "951999557633",
+  appId: "1:951999557633:web:037c3900b4ed065da3d000"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
