031da15008cc1fdbdfd9e5d92381f1d760781592
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-04-24 14:34:43 +0200

    update Register

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSsmQRiKQe403s5Xjsi0T8oDxt67mZeN8",
-  authDomain: "enxhi-chat.firebaseapp.com",
-  projectId: "enxhi-chat",
-  storageBucket: "enxhi-chat.appspot.com",
-  messagingSenderId: "951999557633",
-  appId: "1:951999557633:web:037c3900b4ed065da3d000"
+  apiKey:  import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: "enxhi-chat-6a04f.firebaseapp.com",
+  projectId: "enxhi-chat-6a04f",
+  storageBucket: "enxhi-chat-6a04f.appspot.com",
+  messagingSenderId: "731179700263",
+  appId: "1:731179700263:web:ba9ff4c710d287fb02be02"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
