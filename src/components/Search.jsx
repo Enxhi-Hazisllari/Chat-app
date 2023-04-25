7f8fccff937265fe81ca5dff39520c2b6e44854e
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-04-25 12:46:57 +0200

    add handleSelect

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, setDoc, doc, updateDoc, serverTimestamp, getDoc,} from "firebase/firestore";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 
@@ -30,14 +30,48 @@ const Search = () => {
     e.code === "Enter" && handleSearch();
   };
 
+  const handleSelect = async () => {
+    //check whether the group(chats in firestore) exists, if not create
+    const combinedId =
+    currentUser.uid > user.uid
+      ? currentUser.uid + user.uid
+      : user.uid + currentUser.uid;
+      try {
+        const response = await getDoc(doc(db, "chats", combinedId));
+
+        if (!response.exists()) {
+          //create a chat in chats collection
+          await setDoc(doc(db, "chats", combinedId), { messages: [] });
+          
+          //create user chats
+          await updateDoc(doc(db, "userChats", currentUser.uid), {
+            [combinedId + ".userInfo"]: {
+              uid: user.uid,
+              displayName: user.displayName,
+              photoURL: user.photoURL,
+            },
+            [combinedId + ".date"]: serverTimestamp(),
+          });
+        }
+      }catch(error){}
+
+      setUser(null);
+      setUserName("")
+  };
+
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" placeholder='Search user' onKeyDown={handleKey} onChange={(e)=>setUserName(e.target.value)} />
+        <input 
+          type="text" 
+          placeholder='Search user' 
+          onKeyDown={handleKey} 
+          onChange={(e)=>setUserName(e.target.value)}
+          value={userName}/>
       </div>
       {error && <span>User not found!</span>}
       {user && (
-        <div className="userChat">
+        <div className="userChat" onClick={handleSelect}>
           <img src={user.photoURL} alt="user-image" />
           <div className="userChatInfo">
             <span>{user.displayName}</span>
