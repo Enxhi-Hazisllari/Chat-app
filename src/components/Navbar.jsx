7ea4a9b90cbffd212ee0e74b7020785a12a88920
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-04-25 11:45:35 +0200

    update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from '/image/logo.png';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className='navbar'>
       <img src={logo} alt="logo-chat" className='logo' />
       <div className="user">
-        <img src="" alt="user-image" className='user_image' />
-        <span>Name</span>
+        <img src={currentUser.photoURL} alt="user-image" className='user_image' />
+        <span>{currentUser.displayName}</span>
         <button onClick={()=>signOut(auth)}>Logout</button>
       </div>
     </div>
